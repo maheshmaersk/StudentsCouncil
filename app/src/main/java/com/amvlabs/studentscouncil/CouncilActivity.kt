d1f1e9b34194732681ab226094729cae8c5572bc
Mahesh <70198087+maheshmaersk@users.noreply.github.com>
2022-10-09 12:14:12 +0530

    Delete Functionality Addition Live

--- app/src/main/java/com/amvlabs/studentscouncil/CouncilActivity.kt
+++ app/src/main/java/com/amvlabs/studentscouncil/CouncilActivity.kt
@@ -8,11 +8,15 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.amvlabs.studentscouncil.databinding.ActivityCouncilBinding
 import com.amvlabs.studentscouncil.databinding.ActivityHomeBinding
 import com.amvlabs.studentscouncil.models.UserDetail
 import com.amvlabs.studentscouncil.ui.home.HomeFragment
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
@@ -33,23 +37,26 @@ class CouncilActivity : AppCompatActivity() {
 
         auth = Firebase.auth
         val userDetailList = mutableListOf<UserDetail>()
+        binding.progressBar.visibility = View.VISIBLE
         db.collection("report")
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
                     Log.d(TAG, "${document.id} => ${document.data}")
                     val data: MutableMap<String, Any> = document.data
-                    userDetailList.add(
-                        UserDetail(
-                            category = data["category"] as String,
-                            userID = data["userID"] as String,
-                            description = data["description"] as String,
-                            email = data["email"] as String,
-                            report_description = data["report_description"] as String?,
-                            report_status = data["report_status"] as Long?,
-                            documentID = document.id
+                    if (!(data["isDeleted"] as Boolean)) {
+                        userDetailList.add(
+                            UserDetail(
+                                category = data["category"] as String,
+                                userID = data["userID"] as String,
+                                description = data["description"] as String,
+                                email = data["email"] as String,
+                                report_description = data["report_description"] as String?,
+                                report_status = data["report_status"] as Long?,
+                                documentID = document.id
+                            )
                         )
-                    )
+                    }
                 }
                 loadRecyclerView(userDetailList)
                 Log.d(TAG, "${"Complains Added"} => ${userDetailList.size}")
@@ -73,16 +80,17 @@ class CouncilActivity : AppCompatActivity() {
             startActivity(mIntent)
             finishAffinity()
             return true
-        }
-        else {
+        } else {
             return true
         }
     }
 
-    fun loadRecyclerView(userDetailList: MutableList<UserDetail>) {
+    private fun loadRecyclerView(userDetailList: MutableList<UserDetail>) {
         binding.complaintsList.layoutManager =
             LinearLayoutManager(baseContext, LinearLayoutManager.VERTICAL, false)
         binding.complaintsList.adapter = RecycleUserAdapter(userDetailList)
+        val recycleAdapter = RecycleUserAdapter(userDetailList)
+        binding.progressBar.visibility = View.GONE
 
         if (userDetailList.size > 0) {
             binding.complaintsList.visibility = View.VISIBLE
@@ -91,5 +99,52 @@ class CouncilActivity : AppCompatActivity() {
             binding.noResultGroup.visibility = View.VISIBLE
             binding.complaintsList.visibility = View.GONE
         }
+
+        recycleAdapter.notifyDataSetChanged()
+
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val userDetail: UserDetail = userDetailList[viewHolder.adapterPosition]
+
+                val position = viewHolder.adapterPosition
+
+                userDetailList.removeAt(viewHolder.adapterPosition)
+
+                recycleAdapter.notifyItemRemoved(viewHolder.adapterPosition)
+
+                userDetail.documentID?.let { documentID ->
+                    db.collection("report").document(documentID).update(
+                        mapOf(
+                            "isDeleted" to true,
+                        )
+                    )
+                    Toast.makeText(
+                        binding.complaintsList.context, "Successfully Deleted.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                Snackbar.make(
+                    binding.complaintsList,
+                    "Deleted " + userDetail.email,
+                    Snackbar.LENGTH_LONG
+                ).show()
+//                    .setAction(
+//                        "Undo",
+//                        View.OnClickListener {
+//                            userDetailList.add(position, userDetail)
+//                            recycleAdapter.notifyItemInserted(position)
+//                        }).show()
+            }
+        }).attachToRecyclerView(binding.complaintsList)
+
     }
 }
